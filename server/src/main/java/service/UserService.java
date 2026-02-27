92cecea087f2623e8be2e2e76a11425239d5e2bc
Austin <austinjolong@gmail.com>
2026-02-27 08:47:20 -0700

    finished User Service base implementation

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,6 +4,11 @@ import dataaccess.AuthDataAccess;
 import dataaccess.DataAccessException;
 import dataaccess.UserDataAccess;
 import service.RR_Classes.*;
+import model.UserData;
+import model.AuthData;
+
+import java.util.Objects;
+import java.util.UUID;
 
 public class UserService {
     private final UserDataAccess userDataAccess;
@@ -15,15 +20,38 @@ public class UserService {
         this.authDataAccess = authDataAccess;
     }
 
-    public RegisterResult register(RegisterRequest registerRequest) {
-
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException{
+        UserData userData = userDataAccess.getUser(registerRequest.username());
+        if (userData != null) {
+            throw new DataAccessException("Error: username already taken");
+        } else {
+            userData = new UserData(registerRequest.username(),registerRequest.password(),registerRequest.email());
+            userDataAccess.createUser(userData);
+            String authToken = generateToken();
+            AuthData authData = new AuthData(authToken,registerRequest.username());
+            authDataAccess.createAuth(authData);
+            return new RegisterResult(authData.username(),authToken);
+        }
     }
-    public LoginResult login(LoginRequest loginRequest) {
-
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException{
+        UserData userData = userDataAccess.getUser(loginRequest.username());
+        if (userData == null) {
+            throw new DataAccessException("Username not found");
+        } else if (!Objects.equals(userData.password(), loginRequest.password())){
+            throw new DataAccessException("Password does not match");
+        } else {
+            String authToken = generateToken();
+            authDataAccess.createAuth(new AuthData(authToken,loginRequest.username()));
+            return new LoginResult(loginRequest.username(),authToken);
+        }
     }
 
-    public void logout(LogoutRequest logoutRequest) throws DataAccessException {
+    public void logout(LogoutRequest logoutRequest){
         authDataAccess.deleteAuth(logoutRequest.authToken());
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 
 
